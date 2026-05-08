a6653d60e3f19bec3e30c9d085e3b304efb73635
HlaineNK <hlainenadikhant@gmail.com>
2026-05-08 16:31:20 +1000

    model folder

---
+++ MealPrep/Models/Blogger.swift
@@ -0,0 +1,14 @@
+//
+//  Blogger.swift
+//  MealPrep
+//
+//  Created by Hline Nadi Khant on 8/5/2026.
+//
+
+import Foundation
+
+struct Blogger: Identifiable {
+    let id: UUID
+    let name: String
+    let imageURL: String
+}
