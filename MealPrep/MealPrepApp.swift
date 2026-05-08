a359529e1ce493e691c2a71a258194ac21174246
Enerel <enerelts0011@gmail.com>
2026-05-08 14:19:00 +1000

    Initial Commit

---
+++ MealPrep/MealPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepApp.swift
+//  MealPrep
+//
+//  Created by Enerel Tsolmonbayar on 8/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct MealPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
